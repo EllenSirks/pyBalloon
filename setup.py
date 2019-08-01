40121c1bedb4cac78ffbae843b8d9f79f969c4a5
EllenSirks <ellensirks1995@gmail.com>
2019-08-01 10:27:17 +0100

    Update 01-08-2019

---
+++ setup.py
@@ -0,0 +1,25 @@
+import sys, os
+
+import param_file as p
+
+print('Have you changed to path and folder names in param_file.py?')
+answer = raw_input('y or n: ')
+
+if answer == 'y':
+
+	if not os.path.exists(p.path + p.weather_data_folder + p.GFS_folder):
+		os.mkdir(p.path + p.weather_data_folder + p.GFS_folder)
+	if not os.path.exists(p.path + p.weather_data_folder + p.GEFS_folder):	
+		os.mkdir(p.path + p.weather_data_folder + p.GEFS_folder)
+	if not os.path.exists(p.path + p.elevation_data_folder):
+		os.mkdir(p.path + p.elevation_data_folder)
+	if not os.path.exists(p.path + p.output_folder):
+		os.mkdir(p.path + p.output_folder)
+
+else:
+	print('Please go to param_file.py and change the path to where the pyBalloon folder is located.')
+	sys.exit()
+
+
+
+
