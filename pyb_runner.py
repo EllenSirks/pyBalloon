332ec4da884b5a81076fb3509a713eed033e5ec9
EllenSirks <ellensirks1995@gmail.com>
2019-08-01 11:32:24 +0100

    Update 01-08-2019

--- pyb_runner.py
+++ pyb_runner.py
@@ -29,6 +29,7 @@ def runner(datestr=None, utc_hour=None, loc0=None, balloon=None, params=None, ru
 	# starting location
 	lat0, lon0, alt0 = loc0
 
+	# change altitude if it is underground (not always accurate)
 	if pyb_aux.get_elevation(lon0, lat0) > alt0:
 		alt0 = pyb_aux.get_elevation(lon0, lat0)
 
@@ -41,23 +42,17 @@ def runner(datestr=None, utc_hour=None, loc0=None, balloon=None, params=None, ru
 
 	############################################################################################################ <---- set/create paths
 
-	# initialise paths
-	dir_base = p.path
-	in_dir = dir_base + p.weather_data_folder + p.GFS_folder
-	traj_dir = dir_base + p.output_folder
-	fig_dir = dir_base + p.output_folder
-
 	# determine run number (datestr + no.)
 	if run == None:
 		now_str = str(now.year) + str(now.month).zfill(2) + str(now.day).zfill(2)
-		files = [filename for filename in os.listdir(traj_dir) if now_str in filename]
+		files = [filename for filename in os.listdir(p.path + p.output_folder) if now_str in filename]
 		run = now_str + '_' + str(len(files))
 
-	# create path names with correct run
-	kml_dir = traj_dir + run + '/' + kml_folder
-	params_dir = traj_dir + run + '/'
-	traj_dir += run + '/' traj_folder
-	fig_dir += run + fig_folder + check_figs_folder
+	# create path names
+	base_dir = p.path + p.output_folder + run + '/'
+	kml_dir = base_dir + p.kml_folder
+	traj_dir = base_dir + p.traj_folder
+	fig_dir = base_dir + p.fig_folder + p.check_figs_folder
 
 	# check if the paths exist/make them
 	if not os.path.exists(kml_dir):
@@ -84,36 +79,23 @@ def runner(datestr=None, utc_hour=None, loc0=None, balloon=None, params=None, ru
 	traj_file = traj_dir + 'trajectory_' + datestr + '_' + str(utc_hour) + '_' + str(loc0)
 	kml_fname = kml_dir + datestr + '_' + str(utc_hour) + '_' + str(loc0)
 
-	# check for double file names and add count if there are (usefull for testing drifting/forecasts)
-	count = 2
-	while True:
-		if not os.path.isfile(traj_file + '.dat'):
-			traj_file += '.dat'
-			kml_fname += '.kml'
-			break
-		else:
-			if count == 2:
-				traj_file += '_' + str(count)
-				kml_fname += '_' + str(count)
-			else:
-				ind1 = [pos for pos, char in enumerate(traj_file) if char == '_'][-1]
-				ind2 = [pos for pos, char in enumerate(kml_fname) if char == '_'][-1]
-				traj_file = traj_file[:ind1+1] + str(count)
-				kml_fname = kml_fname[:ind2+1] + str(count)
-			count += 1
+	# determine number of files already in folder (with the same starting params)
+	no = len([filename for filename in os.listdir(traj_dir) if os.path.basename(traj_file) in filename])
+	if no == 0:
+		traj_file += '.dat'
+		kml_fname += '.kml'
+	else:
+		traj_file += '_' + str(no + 1) + '.dat'
+		kml_fname += '_' + str(no + 1) + '.kml'
 
 	# write out trajectory file
 	ascii.write([trajectories['lats'], trajectories['lons'], trajectories['alts'], trajectories['dists'], trajectories['times'], trajectories['speeds'], trajectories['z_speeds'], \
 		trajectories['omegas'], trajectories['temperatures'], trajectories['grid_spreads_u'], trajectories['grid_spreads_v']], traj_file, names=['lats', 'lons', 'alts', 'dists',\
 		 'times', 'speeds', 'z_speeds', 'omegas', 'temps', 'u_spread', 'v_spread'], overwrite=True)
 
-	# ascii.write([trajectories['lats'], trajectories['lons'], trajectories['alts'], trajectories['dists'], trajectories['times'], trajectories['speeds'], trajectories['z_speeds'], \
-	# 	trajectories['omegas'], trajectories['temperatures'], trajectories['grid_spreads_u'], trajectories['grid_spreads_v'], trajectories['sigmas_u'], trajectories['sigmas_v']]\
-	# 	, traj_file, names=['lats', 'lons', 'alts', 'dists', 'times', 'speeds', 'z_speeds', 'omegas', 'temps', 'u_spread', 'v_spread', 'sigmas_u', 'sigmas_v'], overwrite=True)
-
 	# write parameters of this run to file
 	if write_verbose:
-		pyb_io.write_verbose(params_dir=params_dir, params=params, balloon=balloon)
+		pyb_io.write_verbose(params_dir=base_dir, params=params, balloon=balloon)
 
 	# add run info to run_info.txt file
 	pyb_io.write_run_info(add_run_info=add_run_info, run=run, params=params, balloon=balloon)
@@ -130,14 +112,8 @@ def runner(datestr=None, utc_hour=None, loc0=None, balloon=None, params=None, ru
 	timex = trajectories['times'][idx][0]
 
 	# write out file for google-earth
-	# radius = np.sqrt((np.sum(np.absolute(trajectories['grid_spreads_u']))/1000.)**2 + (np.sum(np.absolute(trajectories['grid_spreads_v']))/1000.)**2) + \
-	# np.sqrt((np.sum(np.absolute(trajectories['sigmas_u']))/1000.)**2 + (np.sum(np.absolute(trajectories['sigmas_v']))/1000.)**2)
-
-	# radius = np.sqrt((np.sum(np.absolute(trajectories['grid_spreads_u']))/1000.)**2 + (np.sum(np.absolute(trajectories['grid_spreads_v']))/1000.)**2)
-
 	other_info = [(latx, lonx, altx, 'Burst point', '%.0f minutes, %.0f meters' % (timex, altx))]
-	pyb_io.save_kml(kml_fname, trajectories, other_info=other_info, params=params)
-	# pyb_io.save_kml(kml_fname, trajectories, other_info=other_info, params=params, radius=radius)
+	pyb_io.save_kml(kml_fname, trajectories, other_info=other_info, params=params, radius=5)
 
 	# save figs with interpolation checks
 	if output_figs:
@@ -151,9 +127,11 @@ if __name__ == "__main__":
 
 	time0 = time.time()
 
+	datestr = sys.argv[1]
+	utc_hour = sys.argv[2]
 	loc0 = float(sys.argv[3]), float(sys.argv[4]), float(sys.argv[5])
 
-	runner(datestr=sys.argv[1], utc_hour=sys.argv[2], loc0=loc0, print_verbose=True, write_verbose=True, add_run_info=False, output_figs=False)
+	runner(datestr=datestr, utc_hour=utc_hour, loc0=loc0, print_verbose=True, write_verbose=True)
 
 	sys.stdout.write('\r')
 	sys.stdout.flush()
