c37ed45559e5deba09596da20c98aa675a93c0ec
EllenSirks <ellensirks1995@gmail.com>
2020-03-05 14:45:03 +0000

    Cleaned up the code a little

--- pyb_drifter.py
+++ pyb_drifter.py
@@ -15,33 +15,28 @@ import param_file as p
 
 #################################################################################################################
 
-def drifter(datestr=None, utc_hour=None, loc0=None, balloon=None, params=None, run=None, print_verbose=False, write_verbose=True, add_run_info=False, overwrite=False):
+def drifter(ini_conditions=None, balloon=None, params=None, run=None, overwrite=False, drift_times=None):
 	"""
 	Method to calculate trajectories starting at the same location and time, but have different drift times
 
 	Arguments
 	=========
-	datestr : string
-		Date of initial point
-	utc_hour : float
-		Time of initial point
-	loc0 : floats in tuple
-		(latitude in degrees, longitude in degrees, altitude in km) of initial point
+	ini_conditions : tuple of strings
+		(Date of initial point, Initial time of trajectory, (latitude in degrees, longitude in degrees, altitude in km) of initial point
 	balloon : dict
 		Dictionary of balloon parameters, e.g. burtsradius, mass etc.
 	params : list
 		List of parameters determining how the trajectory is calculated, e.g. with interpolation, descent_only etc.
 	run : string
 		String indicating which run folder the results are to be stored in
-	print_verbose : bool
-		If True, the parameters used will be printed to the command line
-	write_verbose : bool
-		If True, the parameters used are written to a file
-	add_run_info : bool 
-		If True, the parameters used are appended to the run_info.txt file in the Output folder		
 	overwrite : bool
 		If True, overwrite trajectory file with same name in folder
+	drift_time: array
+		Array of several drift times to check
 	"""
+
+	datestr, utc_hour, loc0 = ini_conditions
+	lat0, lon0, alt0 = loc0
 
 	out_dir = p.path + p.output_folder
 
@@ -53,27 +48,22 @@ def drifter(datestr=None, utc_hour=None, loc0=None, balloon=None, params=None, r
 		files = [filename for filename in os.listdir(out_dir) if now_str in filename]
 		run = now_str + '_' + str(len(files))
 
-	descent_only, drift_time, resolution, hr_diff, params, balloon = pyb_io.set_params(params=params, balloon=balloon)
+	descent_only, drift_time, resolution, hr_diff, check_elevation, live, params, balloon = pyb_io.set_params(params=params, balloon=balloon)
 
-	drift_times = np.arange(0., 20., 5.)
-	params[4] = drift_times
+	params[1] = drift_times
 
-	if print_verbose:
-		pyb_io.print_verbose(datestr=datestr, utc_hour=utc_hour, loc0=loc0, params=params, balloon=balloon)
+	pyb_io.print_verbose(datestr=datestr, utc_hour=utc_hour, loc0=loc0, params=params, balloon=balloon)
 
-	if write_verbose:
-
-		params_dir = out_dir + run + '/'
-		if not os.path.exists(params_dir):
-			os.makedirs(params_dir)
-
-		pyb_io.write_verbose(params_dir=params_dir, params=params, balloon=balloon)
+	params_dir = out_dir + run + '/'
+	if not os.path.exists(params_dir):
+		os.makedirs(params_dir)
+	pyb_io.write_verbose(params_dir=params_dir, params=params, balloon=balloon)
 
 	for drift_time in drift_times:
 
-		params[4] = drift_time
+		params[1] = drift_time
 		print('Running drift time: ' + str(drift_time) + ' minutes')
-		pyb_runner.runner(datestr=datestr, utc_hour=utc_hour, loc0=loc0, params=params, balloon=balloon, run=run, add_run_info=add_run_info, overwrite=overwrite)
+		pyb_runner.basic_runner(ini_conditions=ini_conditions, params=params, balloon=balloon, run=run, overwrite=overwrite)
 		print('----------\n')
 
 	pyb_io.merge_kml(datestr=datestr, run=run, params=params, balloon=balloon, drift_times=drift_times)
@@ -87,8 +77,16 @@ if __name__ == '__main__':
 	datestr = sys.argv[1]
 	utc_hour = float(sys.argv[2])
 	loc0 = float(sys.argv[3]), float(sys.argv[4]), float(sys.argv[5])
+	ini_conditions = (datestr, utc_hour, loc0)
 
-	drifter(datestr=datestr, utc_hour=utc_hour, loc0=loc0, print_verbose=True)
+	if len(sys.argv) > 6:
+		run = sys.argv[6]
+	else:
+		run = None
+
+	drift_times = np.arange(0., 40., 10.)
+
+	drifter(ini_conditions=ini_conditions, run=run, drift_times=drift_times)
 
 	print('Total time elapsed: %.1f s' % (time.time() - time0))
 
